4143c41befee2badb289f8b59be397ff49724fa3
Carson Kelley <carsonmkelley@gmail.com>
2018-06-13 17:56:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+function nestedTarget() {
+  return document.querySelector("#nested .target")
+}
+function increaseRankBy(n) {
+  var list = document.querySelectorAll(".ranked-list")
+  for(let i = 0;i < list.length; i++) {
+    list[i].innerHTML = parseInt(list[i].innerHTML) + n
+  }
+}
+function deepestChild() {
+  var top = document.querySelectorAll("div#grand-node div")
+  return top[top.length - 1]
+}
