56cff7f9c03417bdfce3d33b4a531950efd15f2c
Carson Kelley <carsonmkelley@gmail.com>
2018-06-13 17:21:47 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+function nestedTarget() {
+  return document.querySelector("#nested .target")
+  console.log("wha")
+}
+function deepestChild() {
+  var top = document.querySelector("div#grand-node")
+  console.log("what");
+}
