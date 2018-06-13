5813803ca24f0fdbb6dc957ee60db492689c5a3d
Carson Kelley <carsonmkelley@gmail.com>
2018-06-13 17:55:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ function nestedTarget() {
   return document.querySelector("#nested .target")
 }
 function increaseRankBy(n) {
-  
+  var list = document.querySelectorAll(".ranked-list")
+  for(var i =)
 }
 function deepestChild() {
   var top = document.querySelectorAll("div#grand-node div")
