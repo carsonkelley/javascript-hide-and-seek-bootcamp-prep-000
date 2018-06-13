286a7c8bb7596ac8b341b1746ff2691d224f3613
Carson Kelley <carsonmkelley@gmail.com>
2018-06-13 17:54:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.querySelector("#nested .target")
 }
+function increaseRankBy(n) {
+  
+}
 function deepestChild() {
   var top = document.querySelectorAll("div#grand-node div")
   return top[top.length - 1]
